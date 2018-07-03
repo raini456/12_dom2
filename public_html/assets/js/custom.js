e92235ce82d779fc6c150120ff0a2619e39c6238
pr08 <pr08@figd-1711708.figdtk.de>
2018-07-03 11:51:21 +0200

    2nd Commit 03.07.18

--- public_html/assets/js/custom.js
+++ public_html/assets/js/custom.js
@@ -1,16 +1,44 @@
 (function(opts){
-    var backgrounds, icons, canvas, max;
+    var backgrounds, icons, canvas, bgMax, iconsMax, iconActive;
     var init=function(){
         canvas = document.querySelector(opts.DOMCanvas);
         backgrounds = document.querySelectorAll(opts.DOMBackgrounds);
         icons = document.querySelectorAll(opts.DOMIcons);   
         //Events
-        max=backgrounds.length;
-        for(var i= 0; i<max;i++){
-            backgrounds[i].addEventListener('click', setBackground);
-            
+        bgMax=backgrounds.length;
+        for(var i= 0; i<bgMax;i++){
+            backgrounds[i].addEventListener('click', setBackground);            
         }
+        iconsMax=icons.length;
+        for(var i=0; i<iconsMax; i++){
+            icons[i].addEventListener('click', setIconActive);            
+        }
+        //Canvas click drawIcon
+        canvas.addEventListener('click', drawIcon);
+        
     };
+    //iconActive an Canvas anhängen
+    var drawIcon = function (e){ 
+        //Ermittlung der Mousepostione
+        console.log(e.pageX - canvas.offsetLeft);  
+        console.log(e.pageY - canvas.offsetTop);        
+        
+        var c=iconActive.cloneNode();
+        //Position zuweisen
+        
+        c.style.left=(e.pageX - canvas.offsetLeft -40) + 'px';
+        c.style.top=(e.pageY - canvas.offsetTop -50) + 'px';
+        canvas.appendChild(c);        
+    }
+    var setIconActive = function(){        
+        //this.style.boxShadow="0 0 5px green";
+        if(iconActive !== undefined){
+            iconActive.classList.remove('active');
+        }
+        iconActive=this;
+        iconActive.classList.add('active');
+    }
+    
     var setBackground = function(){
         canvas.style.backgroundImage='url("'+this.src +'")';
     }
